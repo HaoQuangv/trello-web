0d8ed8045e6f50de067ef7df5b7230b0fd0be8d1
Quang Hao <quanghao@Quangs-MacBook-Air.local>
2024-01-28 00:03:29 +0700

    dark_light-p2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,53 @@ import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
 
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/system/Box'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const modeSelected = event.target.value
+    setMode(modeSelected)
+    //setAge(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-light-dark-system">Mode</InputLabel>
+      <Select
+        labelId="demo-select-light-dark-system"
+        id="demo-select-light-dark-system"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <SettingsBrightnessIcon /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
@@ -22,6 +69,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <br />
       <ModeToggle />
       <br />
       <div>BuiHaoQuang</div>
