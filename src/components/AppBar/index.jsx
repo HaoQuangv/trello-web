b5b9491a99faad1a53f8f289345883ba375c7e42
Quang Hao <quanghao@192.168.1.5>
2024-02-03 09:08:03 +0700

    code app bar p3

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -24,7 +24,9 @@ function AppBar() {
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      gap: 2,
+      overflowX: 'auto'
     }}>
       <Box sx={{
         display: 'flex',
@@ -41,12 +43,13 @@ function AppBar() {
           <Typography sx={{ fontSize: '1.2rem', fontWeight: 'Bold', color: 'primary.main' }}>Trello</Typography>
         </Box>
 
-        <Workspaces />
-        <Recent />
-        <Starred />
-        <Templates />
-
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+          <Workspaces />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
       </Box>
 
       <Box sx={{
@@ -54,7 +57,7 @@ function AppBar() {
         alignItems: 'center',
         gap: 2
       }}>
-        <TextField id="outlined-search" label="Search..." type="search" size='small'/>
+        <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: '120px' }}/>
         <ModeSelect />
         <Tooltip title="Notification">
           <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer', color: 'primary.main' }}>
