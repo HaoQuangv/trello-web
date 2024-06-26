23dbe62035ebd6da898b13629c1c20e3633a6c58
Quang Hao <quanghao@Quangs-MacBook-Air.local>
2024-06-26 09:57:20 +0700

    mongodb_aggregate

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,8 +3,9 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
+
+//import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
@@ -13,7 +14,7 @@ function Board() {
   useEffect(() => {
     //Tam thoi fix cung boardId, flow chuan chinh ve sau khi hoc nang cao truc tiep voi minh la chung ta se su dung truc tiep
     // react-router-dom de lay chuan boardId tu URL ve
-    const boardId = '6677f48c862abe80b44f9b5d'
+    const boardId = '667a69c5324eb1ce208001eb'
     // Call API
     fetchBoardDetailsAPI(boardId).then(board => {
       setBoard(board)
